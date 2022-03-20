2f319854453ec719b23051c821ba872ea3cf0eb8
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-19 22:34:33 -0700

    added start of pseudo code

--- pseudo_code_format.js
+++ pseudo_code_format.js
@@ -1,3 +1,7 @@
 ../pseudo_code_format.js
 
-//
+//  1. Format starting HTML files (going to need 2 bc have extra high score pg) 
+//      -need to be able to click to view high  score pg & time keeper on  top of page
+//      -Start Page w/ explanation of task & button to click to start quiz
+//      -2nd  pg default shows Highscores and includes button to return to starting pg & to clear high scores
+
