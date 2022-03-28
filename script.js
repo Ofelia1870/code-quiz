d5c8bf2a149f231d748c59c9eaaa9a236094babf
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-27 23:12:01 -0700

    added eventListener after clicking start button

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var timerEL = document.getElementById("countdown");
 
-// function countdown() {
-    var secondsLeft = 90;
+var startButton = document.querySelector(".start-quiz-button");
 
-    var timeInterval = setInterval(function () {
-        // if (secondsLeft > 1) {
-        //     timerEL.textContent = secondsLeft + ' secs';
+// console.log(startButton);
+
+    
+
+    startButton.addEventListener("click", function(){
+        var secondsLeft = 90;
+
+        var timeInterval = setInterval(function () {
+        
             secondsLeft--;
             timerEL.textContent = secondsLeft;
             if(secondsLeft <= 0)
-        // } else if (secondsLeft === 1) {
-        //     timerEL.textContent = secondsLeft + 'sec';
-        //     secondsLeft--;
-        // } else {
-        //   timerEL.textContent = '';
-          
+    
           clearInterval(timeInterval);
         }, 1000);
+        
+    });
+           
     
 
-    console.log(countdown);
+
