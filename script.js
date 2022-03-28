4b62e6050c1ec7843496f15b41cee8c649ddd516
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-28 14:39:02 -0700

    added alert box when timer runs out

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ var timerEL = document.getElementById("countdown");
 
 var startButton = document.querySelector(".start-quiz-button");
 
-var h3El = document.createElement("h3")
+function question(text, choices) {
+    this.text = text;
+    this.choices =choices
+}
+
+// var h3El = document.createElement("h3")
+var codeQuestion =  [
+    new question("question1", ["answerA", "answerB"])
+]
+
 
 // console.log(startButton);
 
     function startQuiz() {
-        document.querySelector(".container").innerHTML =  "Question 1";
+        document.querySelector(".container").innerHTML =  codeQuestion;
         //  document.querySelector(".container").innerHTML = h3El;
         //  h3El.textContent = "Question 1";
     }
@@ -20,9 +29,12 @@ var h3El = document.createElement("h3")
         
             secondsLeft--;
             timerEL.textContent = secondsLeft;
-            if(secondsLeft <= 0)
     
-          clearInterval(timeInterval);
+            if(secondsLeft === 0) {
+              alert("Game Over!! You ran out of time.")
+
+            clearInterval(timeInterval);
+          }
         }, 1000);
 
     startButton.addEventListener("click", startQuiz)
