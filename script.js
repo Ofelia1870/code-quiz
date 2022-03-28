83a16e37198fc122a08a95ba5f42fb19d3ca818a
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-27 18:24:28 -0700

    updated timer

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var timerEL = document.getElementById('countdown');
+var timerEL = document.getElementById("countdown");
 
-function countdown() {
+// function countdown() {
     var secondsLeft = 90;
 
-    var timeInterval = setInterval(function() {
-        if (secondsLeft > 1) {
-            timerEL.textContent = secondsLeft + ' secs';
+    var timeInterval = setInterval(function () {
+        // if (secondsLeft > 1) {
+        //     timerEL.textContent = secondsLeft + ' secs';
             secondsLeft--;
-        } else if (secondsLeft === 1) {
-            timerEL.textContent = secondsLeft + 'sec';
-            secondsLeft--;
+            timerEL.textContent = secondsLeft;
+            if(secondsLeft <= 0)
+        // } else if (secondsLeft === 1) {
+        //     timerEL.textContent = secondsLeft + 'sec';
+        //     secondsLeft--;
         // } else {
         //   timerEL.textContent = '';
           
-        //   clearInterval(timeInterval);
-
-            }
+          clearInterval(timeInterval);
         }, 1000);
-    }
+    
+
+    console.log(countdown);
