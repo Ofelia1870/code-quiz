cd56e7f139151e5aa1f545c91125ccf2e928aedf
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-29 13:23:13 -0700

    commented out quiz questions hide/show

--- script.js
+++ script.js
@@ -2,27 +2,12 @@ var timerEL = document.getElementById("countdown");
 
 var startButton = document.querySelector(".start-quiz-button");
 
-function question(text, choices) {
-    this.text = text;
-    this.choices =choices
-}
 
-// var h3El = document.createElement("h3")
-var codeQuestion =  [
-    new question("question1", ["answerA", "answerB"])
-]
+    // document.querySelector("#quiz-section").style.display = "none";
 
-
-// console.log(startButton);
-
-    // function startQuiz() {
-    //     document.querySelector(".container").innerHTML =  codeQuestion;
-    // }
-    document.querySelector("#quiz-section").style.display = "none";
-
-    document.querySelector(".start-quiz-button").onclick = function() {
-        document.querySelector("#quiz-section").style.display = "block";
-        document.querySelector(".container").style.display = "none";
+    // document.querySelector(".start-quiz-button").onclick = function() {
+    //     document.querySelector("#quiz-section").style.display = "flex";
+    //     document.querySelector(".container").style.display = "none";
 
     }
 
